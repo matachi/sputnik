d0c0e3b8fbe53467ed68f34adb646e4a37a94e10
MaTachi <dannie92@gmail.com>
2014-01-07 22:06:25 +0100

    Add an about page

--- sputnik/urls.py
+++ sputnik/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^', include('podcasts.urls', 'podcasts')),
     url(r'^', include('news.urls', 'news')),
     url(r'^', include('users.urls', 'users')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
