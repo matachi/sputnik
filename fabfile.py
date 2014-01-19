d0710fe6a31271f5ba7a482decd0ba1cd02cd437
MaTachi <dannie92@gmail.com>
2014-01-19 13:00:42 +0100

    Fix error in fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -14,7 +14,7 @@ def setup_dev():
     with cd('"{}"'.format(os.path.dirname(__file__))):
         run('python3 manage.py syncdb')
         run('python3 manage.py loaddata sample_podcasts')
-        update_podcasts()
+        run('python3 manage.py updatepodcasts')
         run('python3 manage.py fetchepisodes')
         run('python3 manage.py update_index')
 
