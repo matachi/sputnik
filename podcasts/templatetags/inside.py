945a194cdde0d47ba9a83e7f6970bb684adbefc3
MaTachi <dannie92@gmail.com>
2014-03-09 22:51:22 +0100

    Fix the inside.py template tag so it works

--- podcasts/templatetags/inside.py
+++ podcasts/templatetags/inside.py
@@ -4,4 +4,4 @@ register = template.Library()
 
 @register.filter()
 def inside(value, collection):
-    return value in collection if isinstance(collection, list) else False
+    return value in collection
