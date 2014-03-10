49af01e1fca3ad824ef7e27536ff02633976821e
MaTachi <dannie92@gmail.com>
2014-03-11 00:08:46 +0100

    Make it easier to reference podcasts and episodes in news items in the admin panel

--- news/admin.py
+++ news/admin.py
@@ -4,5 +4,6 @@ from news.models import NewsItem
 
 class NewsItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'published')
+    filter_horizontal = ('referenced_podcasts', 'referenced_episodes')
 
 admin.site.register(NewsItem, NewsItemAdmin)
