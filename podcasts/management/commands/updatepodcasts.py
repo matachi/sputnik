137b834f166eea13781311b1f050f0eaf2e1d610
MaTachi <dannie92@gmail.com>
2014-03-11 16:47:50 +0100

    Make it optional to only update metadata of new podcasts

--- podcasts/management/commands/updatepodcasts.py
+++ podcasts/management/commands/updatepodcasts.py
@@ -1,10 +1,22 @@
 from django.core.management import BaseCommand
+from optparse import make_option
 from podcasts.models import Podcast
 
 
 class Command(BaseCommand):
     help = 'Initialize podcasts with data from their RSS feeds'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--only-new',
+            action='store_true',
+            dest='only_new',
+            default=False,
+            help='Only update new podcasts'
+        ),
+    )
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         for podcast in Podcast.objects.all():
+            if options['only_new'] and podcast.title != "":
+                continue
             podcast.update()
