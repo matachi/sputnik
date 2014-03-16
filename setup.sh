9ffdbad57052b90c9ae85683f37b7fec95741210
MaTachi <dannie92@gmail.com>
2014-03-16 13:13:16 +0100

    Update the setup.sh file

--- setup.sh
+++ setup.sh
@@ -2,7 +2,13 @@
 
 # Python
 echo -e "\ndeb http://http.debian.net/debian testing main" >> /etc/apt/sources.list
-echo -e "Package: *\nPin: release a=testing\nPin-Priority: -10" > /etc/apt/preferences
+echo "Package: *
+Pin: release a=stable
+Pin-Priority: 600
+
+Package: *
+Pin: release a=testing
+Pin-Priority: 550" > /etc/apt/preferences
 apt-get update
 apt-get install -y -t testing python3 python3-pip
 
@@ -15,23 +21,23 @@ source env/bin/activate
 apt-get install -y libjpeg-dev zlib1g-dev
 pip install Pillow
 apt-get install -y -t testing libxml2-dev libxslt1-dev
-pip3 install lxml
-pip3 install django
-pip3 install feedparser
-pip3 install dateutils
-pip3 install django-allauth
-pip3 install djangorestframework
-pip3 install django-widget-tweaks
-pip3 install django-haystack Whoosh
-pip3 install beautifulsoup4
-pip3 install django-debug-toolbar
+pip install lxml
+pip install django
+pip install feedparser
+pip install dateutils
+pip install django-allauth
+pip install djangorestframework
+pip install django-widget-tweaks
+pip install django-haystack Whoosh
+pip install beautifulsoup4
+pip install django-debug-toolbar
 apt-get install csstidy
-pip3 install slimit django-pipeline
+pip install slimit django-pipeline
 
 # PostgreSQL
 apt-get install -y postgresql postgresql-client
 apt-get install libpq-dev
-pip3 install psycopg2
+pip install psycopg2
 /etc/init.d/postgresql start &&\
     sudo -u postgres psql --command "CREATE USER mypguser WITH PASSWORD 'pass';" &&\
     sudo -u postgres createdb -O mypguser mydb
@@ -43,7 +49,7 @@ ln -s /sputnik/conf/nginx.conf /etc/nginx/sites-enabled/
 
 # uWSGI
 apt-get install libpcre3-dev
-pip3 install uwsgi
+pip install uwsgi
 chown -R www-data:www-data `pwd`
 mkdir /var/log/uwsgi/
 touch /var/log/uwsgi/log.log
