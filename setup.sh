f8547d0c3574c7c01ebd7f0b04316d75b49a65f1
MaTachi <dannie92@gmail.com>
2014-06-15 15:40:34 +0200

    Change deployment structure and setup file to Ubuntu 14.04

--- setup.sh
+++ setup.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-# Python
-echo -e "\ndeb http://http.debian.net/debian testing main" >> /etc/apt/sources.list
-echo "Package: *
-Pin: release a=stable
-Pin-Priority: 600
+# Init
+aptitude update
+aptitude full-upgrade -y
+adduser deploy
+adduser deploy sudo
+sudo -u deploy mkdir -p /home/deploy/sputnik
+cd /home/deploy/sputnik
 
-Package: *
-Pin: release a=testing
-Pin-Priority: 550" > /etc/apt/preferences
-apt-get update
-apt-get install -y -t testing python3 python3-pip
+# Python
+aptitude install python3-pip
 
 # Virtualenv
 pip3 install virtualenv
-virtualenv -p /usr/bin/python3 env
+sudo -u deploy virtualenv env
 source env/bin/activate
 
 # Django site dependencies
-apt-get install -y libjpeg-dev zlib1g-dev
+aptitude install -y libjpeg-dev zlib1g-dev
 pip install Pillow
-apt-get install -y -t testing libxml2-dev libxslt1-dev
+
+aptitude install -y libxml2-dev libxslt1-dev
 pip install lxml
+
 pip install django
 pip install feedparser
 pip install dateutils
@@ -31,30 +32,40 @@ pip install django-widget-tweaks
 pip install django-haystack Whoosh
 pip install beautifulsoup4
 pip install South
-pip install django-debug-toolbar
-apt-get install csstidy
-pip install slimit django-pipeline
+#pip install django-debug-toolbar
+
+#apt-get install csstidy
+#pip install slimit
+pip install django-pipeline
+
+chown -R deploy:deploy env
+
+# Django
+python3 manage.py collectstatic
 
 # PostgreSQL
-apt-get install -y postgresql postgresql-client
-apt-get install libpq-dev
+aptitude install -y postgresql postgresql-client
+aptitude install -y libpq-dev
 pip install psycopg2
 /etc/init.d/postgresql start &&\
     sudo -u postgres psql --command "CREATE USER mypguser WITH PASSWORD 'pass';" &&\
     sudo -u postgres createdb -O mypguser mydb
 
 # Nginx
-apt-get intall -y nginx
-/etc/init.d/nginx start
-ln -s /sputnik/conf/nginx.conf /etc/nginx/sites-enabled/
+aptitude install -y nginx
+service nginx start
+ln -s /home/deploy/sputnik/conf/nginx.conf /etc/nginx/sites-enabled/
 
 # uWSGI
-apt-get install libpcre3-dev
-pip install uwsgi
-chown -R www-data:www-data `pwd`
-mkdir /var/log/uwsgi/
-touch /var/log/uwsgi/log.log
-chown -R www-data:www-data /var/log/uwsgi/
+sudo -u deploy mkdir -p /home/deploy/uwsgi
+cd /home/deploy/uwsgi
 
-# Django
-python3 manage.py collectstatic
+deactivate
+sudo -u deploy virtualenv env
+source env/bin/activate
+
+aptitude install -y libpcre3-dev
+pip install uwsgi
+chown -R deploy:deploy env
+
+sudo -u deploy mkdir log
