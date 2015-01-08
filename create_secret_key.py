c7f95ca01dc40a53d05c91faf5c5b03af97dfffe
MaTachi <dannie92@gmail.com>
2015-01-08 11:25:40 +0100

    Fix a wrong path

--- create_secret_key.py
+++ create_secret_key.py
@@ -7,7 +7,7 @@ chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 secret_key = get_random_string(50, chars)
 
 current_file_dir = path.dirname(path.abspath(__file__))
-settings_file_name = path.join(current_file_dir, 'hydra', 'settings.py')
+settings_file_name = path.join(current_file_dir, 'sputnik', 'settings.py')
 
 # Solution found on http://stackoverflow.com/a/2962828/595990
 for line in fileinput.input(settings_file_name, inplace=True):
